48ca39f3c466938f0beb66aa98e48bebbe85c29d
Ali TOKMAKCI <alitokmakci@outlook.com>
2023-08-08 20:11:08 +0300

    changed paper component

--- components/Paper.tsx
+++ components/Paper.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Paper({children, title}: {children: React.ReactNode, title: string}) {
   return (
-    <div className='card p-40 mt-15 text-center'>
+    <div className='bg-white p-40 border-radius-2 mt-15 text-center'>
         <h1 className="mb-15 fw-500 fs-4">{title}</h1>
         {children}
     </div>
